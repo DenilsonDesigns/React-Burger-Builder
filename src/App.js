f856f5febf93c83221427461279fde04caf4b1e7
DenilsonDesigns <daniellanguilller@gmail.com>
2018-08-31 11:16:51 +1000

    added order output from firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route } from 'react-router-dom';
-
+import Orders from './containers/Orders/Orders';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './containers/Checkout/Checkout';
@@ -12,6 +12,7 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
         </Layout>
       </div>
     );
